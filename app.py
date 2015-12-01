b657bc3a48f98a403eee2bd454cbb5d4f9fa33ce
johnr-spindance <johnr@spindance.com>
2015-12-01 15:45:41 -0500

    WTF

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "<h1>John is THE ServerNinja... deal with it!</h1><br><br>What up?"
+    return "<h1>John is the ServerNinja... deal with it!</h1><br><br>What up?"
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
