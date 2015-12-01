eb5d25284ef2c562644c704c8101c717f96449a7
johnr-spindance <johnr@spindance.com>
2015-12-01 15:36:43 -0500

    Fixing wording

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "<h1>John is THE ServerNinjs... deal with it!</h1>"
+    return "<h1>John is THE ServerNinja... deal with it!</h1><br><br>What up?"
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
